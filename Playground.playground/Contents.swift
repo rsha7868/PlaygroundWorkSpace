943183fc75c99fb5ff1760a63f50e04f2aa88f8b
Sharp <rsha7868@740s-pl-127.local>
2017-09-25 13:04:04 -0600

    Save the unfinnished work.

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -5,13 +5,11 @@ import UIKit
 
 
 
-let mathexpression = NSExpression(format: "10 * 3")
-let mathValue = mathExpression.expressionValueWithObject(nil, context: nil) as? Int
-// 1
-    print(expression.expressioValueWithObject(nil, context: nil))
-//print "30"
-
 var myName :String
 var otherName = "Works"
 var thisName :String = "dog"
 var str = "Hello, playground"
+
+
+
+
