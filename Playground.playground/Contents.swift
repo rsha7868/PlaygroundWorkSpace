d838b458358d6a2223dc34b96f06cf35c2a9e218
Sharp <rsha7868@740s-pl-127.local>
2017-09-25 13:50:53 -0600

    learning more data.

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -10,6 +10,19 @@ var otherName = "Works"
 var thisName :String = "dog"
 var str = "Hello, playground"
 
+var letter = "s"
+var number = 100
+var sentance = "The \(letter)peed of a rocket is \(number)mph"
+
+var speed = 90
+var age = 100
+var rate = age/Int (speed)
+
+
+
+
+
+
 
 
 
