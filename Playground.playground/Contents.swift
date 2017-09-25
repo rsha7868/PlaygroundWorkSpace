d13c413dd4b83b1a489f99a428792446351439e9
Sharp <rsha7868@740s-pl-127.local>
2017-09-25 12:41:27 -0600

    Math is geat when it is hard.

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -2,14 +2,16 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+
+
+let mathexpression = NSExpression(format: "10 * 3")
+let mathValue = mathExpression.expressionValueWithObject(nil, context: nil) as? Int
+// 1
+    print(expression.expressioValueWithObject(nil, context: nil))
+//print "30"
 
 var myName :String
 var otherName = "Works"
 var thisName :String = "dog"
-10 = (3*3)+1
-let expression = NSExpression(format:"a = (b * c)"
-var a = 30
-var b = 9
-var c = 3
-
+var str = "Hello, playground"
