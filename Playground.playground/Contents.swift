40df1af8c0f4f1f1bbd44f629db6d0116c7eb188
Sharp <rsha7868@740s-pl-127.local>
2017-09-25 12:22:16 -0600

    Trying to find out ways to do math in swift?So hard but fun.

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -3,3 +3,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myName :String
+var otherName = "Works"
+var thisName :String = "dog"
+10 = (3*3)+1
+let expression = NSExpression(format:"a = (b * c)"
+var a = 30
+var b = 9
+var c = 3
+
