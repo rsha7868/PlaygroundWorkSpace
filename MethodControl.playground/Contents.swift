3d3ed68c2ff199a955a9434e8a1862793a2da112
Sharp <rsha7868@740s-pl-127.local>
2017-09-27 11:55:22 -0600

    The new way to use  control and method.

---
+++ MethodControl.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+public func isReallySimple() -> Void
+{
+    print("It will be warn this weekened!!!! Yay for 70+ dgrees")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+    let answer = "My name is not" + name
+    print(answer)
+}
+aBitLessSimple(name:"slim shady")
+let words = "Marshall Matters"
+aBitLessSimple(name :)
