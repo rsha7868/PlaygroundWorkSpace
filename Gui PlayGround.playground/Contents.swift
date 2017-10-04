51c91961ad9499d0dcdde2c53ae0fa36474dd4a1
Sharp <rsha7868@740s-pl-127.local>
2017-10-04 13:54:24 -0600

    Wow Art is so powerful...

---
+++ Gui PlayGround.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Working with Gui."
+
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
+
+myFirstButton.setTitle("#001", for: .normal)
+myFirstButton.backgroundColor = UIColor(red:0.30, green:0.70, blue:0.10, alpha:0.10)
+
+var myMathButton = UIButton(frame: CGRect(x:100, y:1000, width:3000, height:300))
+myMathButton.setTitle("hahaha", for: .normal)
+
+
