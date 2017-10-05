2163c48674403390b7639a4fb67463dfe6602bdc
Sharp <rsha7868@740s-pl-127.local>
2017-10-05 11:44:29 -0600

    More and More Gui buttons.

--- Gui PlayGround.playground/Contents.swift
+++ Gui PlayGround.playground/Contents.swift
@@ -9,7 +9,8 @@ var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
 myFirstButton.setTitle("#001", for: .normal)
 myFirstButton.backgroundColor = UIColor(red:0.30, green:0.70, blue:0.10, alpha:0.10)
 
-var myMathButton = UIButton(frame: CGRect(x:100, y:1000, width:3000, height:300))
+var myMathButton = UIButton(frame: CGRect(x:100, y:10, width:300, height:30))
 myMathButton.setTitle("hahaha", for: .normal)
+myMathButton.backgroundColor = UIColor(red:0.15, green:0.35, blue:0.65, alpha:0.90)
 
 
